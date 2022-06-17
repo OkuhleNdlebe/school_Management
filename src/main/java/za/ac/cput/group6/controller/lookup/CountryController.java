14b5a0b64629cfe482befb75a6b6012ecaaa62bd
OkuhleNdlebe <218338619@mycput.ac.za>
2022-06-17 23:14:51 +0200

    service and controllers

--- src/main/java/za/ac/cput/group6/controller/lookup/CountryController.java
+++ src/main/java/za/ac/cput/group6/controller/lookup/CountryController.java
@@ -1,4 +1,39 @@
 package za.ac.cput.group6.controller.lookup;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.group6.domain.lookup.Country;
+import za.ac.cput.group6.service.lookup.impl.CountryServiceImpl;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/country")
 public class CountryController {
+    @Autowired
+    private CountryServiceImpl countryService;
+
+    @GetMapping("/getall")
+    public List<Country> getAll(){
+        return countryService.getAll();
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestBody Country country){
+        countryService.delete(country.getName());
+    }
+    @PutMapping("/update")
+    public Country update(@RequestBody Country country){
+        return countryService.update(country);
+    }
+    @GetMapping("/read/{name}")
+    public Optional<Country> read(@PathVariable String name) {
+        return countryService.read(name);
+    }
+
+    @PostMapping("/create")
+    public Country create(@RequestBody Country country){
+        return countryService.create(country);
+    }
+
 }
--- src/main/java/za/ac/cput/group6/service/IService.java
+++ src/main/java/za/ac/cput/group6/service/IService.java
@@ -1,9 +1,9 @@
 package za.ac.cput.group6.service;
-
+import java.util.Optional;
 public interface IService<T, ID> {
 
     T create(T t);
-    T read(ID id);
+  Optional < T> read(ID id);
     T update(T t);
     void delete(ID id);
 }
